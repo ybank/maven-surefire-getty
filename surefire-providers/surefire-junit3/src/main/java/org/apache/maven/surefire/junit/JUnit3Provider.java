ef14dfcda956734bfd95facb8840d72921355128
Y. Yan <dev@ybank.net>
2016-02-04 05:17:12 -0800

    Use JUnit4 facade everywhere

--- surefire-providers/surefire-junit3/src/main/java/org/apache/maven/surefire/junit/JUnit3Provider.java
+++ surefire-providers/surefire-junit3/src/main/java/org/apache/maven/surefire/junit/JUnit3Provider.java
@@ -75,7 +75,9 @@ public class JUnit3Provider
          * Step 1 - output for getty use the runner and all test classes
          */
         List<String> allTestFiles = ((DefaultScanResult) scanResult).getFiles();
-        String interested_output = "__for__getty__ __junit3__ junit.textui.TestRunner";  // prefix length = 15
+//        String interested_output = "__for__getty__ __junit3__ junit.textui.TestRunner";  // prefix length = 15
+        // As long as JUnit4 is on the patch, such facade will work for JUnit3 test cases
+        String interested_output = "__for__getty__ __junit4__ org.junit.runner.JUnitCore";  // prefix length = 15
 		for (String testClass :  allTestFiles)
 			interested_output += (" " + testClass);
 		System.out.println(interested_output);
